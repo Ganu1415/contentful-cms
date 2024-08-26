ff98d562bf294dc1450f682acb0dd862819feeba
Ganesh Vhanmane <mr.vga1415@gmail.com>
2024-08-26 10:15:34 +0530

    initial commit

---
+++ src/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Succulents thundercats gastropub cred bicycle rights retro twee
+            small batch helvetica tacos. Same vice sriracha DIY, fingerstache
+            vexillologist you probably haven't heard of them pickled
+            letterpress.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
